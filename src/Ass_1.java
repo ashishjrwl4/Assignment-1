a7c9ef3ced2cd0e1ce32e58c2053c9ab03779a4f
Ashish Jariwal <ashishjrwl4@yahoo.com>
2016-08-03 10:20:16 +1200

    Two Switch case included

--- src/Ass_1.java
+++ src/Ass_1.java
@@ -3,37 +3,65 @@ public class Ass_1 {
 
 	public static void main(String[] args) {
 		Product_detail obj1 = new Product_detail();
-     System.out.println("Enter the product details");
-     Scanner user_input = new Scanner(System.in);
-     
-     System.out.println("Enter the Product Name");
-     obj1.Product_name = user_input.next();
-     
-     System.out.println("Enter the Product Code");
-     obj1.Product_code = user_input.next();
-     
-     System.out.println("Enter the Product Brand");
-     obj1.Product_brand = user_input.next();
-     
-     System.out.println("Enter the Product Cost");
-     obj1.product_cost= user_input.next();
-     
-     System.out.println("To know the prodect details press 0");
-     
-     int j;
+		
+		
+        Scanner user_input = new Scanner(System.in);
+    
+    System.out.println("To enter the product detail press 0");
+    
+        
+        
+        int j;
      j = user_input.nextInt();
      
      switch(j){
+     
      case 0 :
-    	 System.out.println("Name of product is " + obj1.Product_name);
-    	 System.out.println("Code of product is " + obj1.Product_code);
-    	 System.out.println("Brand of product is " + obj1.Product_brand);
-    	 System.out.println("Cost of product is " + obj1.product_cost);
-    	 break;
     	 
+         
+         System.out.println("Enter the Product Name");
+         obj1.Product_name = user_input.next();
+         
+         System.out.println("Enter the Product Code");
+         obj1.Product_code = user_input.next();
+         
+         System.out.println("Enter the Product Brand");
+         obj1.Product_brand = user_input.next();
+         
+         System.out.println("Enter the Product Cost");
+         obj1.product_cost= user_input.next();
+         
+         break;
+    
+  
+     
+         
+         
      default :
     	 System.out.println("Wrong Input");
     	 }
 
-	}
+     System.out.println("to know the product detail press 0");
+     
+     
+     int k;
+  k = user_input.nextInt();
+  
+  switch(k){
+  
+  case 0 :
+ 	 System.out.println("name of product is :" + obj1.Product_name);
+ 	 System.out.println("code of product is:" + obj1.Product_code);
+ 	 System.out.println("brand of product is:" + obj1.Product_brand);
+ 	 System.out.println("cost of product is :" + obj1.product_cost);
+      
+      
+     break;
+      
+  default :
+ 	 System.out.println("Wrong Input");
+      
+     
+    }
+}
 }
