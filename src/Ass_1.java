098eca852c7d67e2a06151977c2acc88f2730d3b
Ashish Jariwal <ashishjrwl4@yahoo.com>
2016-08-03 09:56:04 +1200

    Successfully compiled the code

---
+++ src/Ass_1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner; 
+public class Ass_1 {
+
+	public static void main(String[] args) {
+		Product_detail obj1 = new Product_detail();
+     System.out.println("Enter the product details");
+     Scanner user_input = new Scanner(System.in);
+     
+     System.out.println("Enter the Product Name");
+     obj1.Product_name = user_input.next();
+     
+     System.out.println("Enter the Product Code");
+     obj1.Product_code = user_input.next();
+     
+     System.out.println("Enter the Product Brand");
+     obj1.Product_brand = user_input.next();
+     
+     System.out.println("Enter the Product Cost");
+     obj1.product_cost= user_input.next();
+     
+     System.out.println("To know the prodect details press 0");
+     
+     int j;
+     j = user_input.nextInt();
+     
+     switch(j){
+     case 0 :
+    	 System.out.println("Name of product is " + obj1.Product_name);
+    	 System.out.println("Code of product is " + obj1.Product_code);
+    	 System.out.println("Brand of product is " + obj1.Product_brand);
+    	 System.out.println("Cost of product is " + obj1.product_cost);
+    	 break;
+    	 
+     default :
+    	 System.out.println("Wrong Input");
+    	 }
+
+	}
+}
---
+++ src/Product_detail.java
@@ -0,0 +1,9 @@
+
+public class Product_detail {
+ String Product_name;
+ String Product_code;
+ String Product_brand;
+ String product_cost;
+ 
+}
+
